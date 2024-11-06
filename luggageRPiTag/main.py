e5dd314930fec4aa43516c24cce70df67b1c0188
Albert LAU <32214383+albertlau128@users.noreply.github.com>
2024-11-06 14:37:45 +0000

    Optimise flash and modularise

--- luggageRPiTag/main.py
+++ luggageRPiTag/main.py
@@ -2,13 +2,17 @@ import requests
 import time
 import uuid
 import RPi.GPIO as GPIO
+from datetime import datetime, timedelta
 
 domain = "http://localhost:8080"  # Will point to Intranet server
 
 # GPIO pin configuration
 RED_PIN = 17
 GREEN_PIN = 27
-
+FLASH = {
+    'seconds':10,
+    "delay": 0.5,  # Delay between lightbulb on and off
+}
 # Setup GPIO mode
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RED_PIN, GPIO.OUT)
@@ -24,7 +28,9 @@ def get_colour():
     try:
         response = requests.get(f"{domain}/pincolor?pinid={get_pin_id()}")
         response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
-        return response.color  # Expecting a color in one in  ["red", "green", "yellow"]
+        return response.json().get(
+            "color", "error"
+        )  # Expecting a color in one in  ["red", "green", "yellow", "error"]
 
     except requests.exceptions.RequestException as e:
         return "error"  # Return "error" to indicate a failure
@@ -44,12 +50,20 @@ def set_color(color):
         GPIO.output(GREEN_PIN, GPIO.HIGH)
 
 
-def flash_red(times=5, delay=1):
-    for _ in range(times):
+def flash_red(total_seconds):
+    end_time = datetime.now() + timedelta(seconds=total_seconds)
+    while datetime.now() < end_time:
         GPIO.output(RED_PIN, GPIO.HIGH)
-        time.sleep(delay)
+        time.sleep(FLASH.delay)
         GPIO.output(RED_PIN, GPIO.LOW)
-        time.sleep(delay)
+        time.sleep(FLASH.delay)
+
+
+def get_colour_delay_period():
+    # As an improvement
+    # Delay period can be dynamically adjusted (reduced when close to EOBT) in accordance to time till EOBT
+    # Default is set to 60 seconds
+    return 60 # seconds
 
 
 # Example usage
@@ -58,11 +72,10 @@ if __name__ == "__main__":
         while True:
             color = get_colour()
             if color == "error":
-                flash_red()  # Flash red on error
+                flash_red(FLASH.seconds)  # Flash red on error
             else:
                 set_color(color)  # Set the color of the LED
-
-            time.sleep(10)  # Wait for 10 seconds
+                time.sleep(get_colour_delay_period())
     except KeyboardInterrupt:
         pass
     finally:
