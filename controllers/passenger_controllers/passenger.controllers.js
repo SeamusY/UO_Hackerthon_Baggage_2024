2d7196b4dfed570af50828ea4a7a0a9317b2c8a5
Shay <seamus.y2016@gmail.com>
2024-11-06 17:52:14 +0800

    initial commit

---
+++ controllers/passenger_controllers/passenger.controllers.js
@@ -0,0 +1,14 @@
+const getPassenger = (ctx) => {
+  ctx.body = [];
+  ctx.status = 200;
+};
+
+const updatePassenger = (ctx) => {
+  ctx.body = []; // Send to backend for edit.
+  ctx.status = 200;
+};
+
+module.exports = {
+  getPassenger,
+  updatePassenger
+};
