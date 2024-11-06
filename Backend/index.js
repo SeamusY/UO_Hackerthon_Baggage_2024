e618e2fc39c457c16e0568aef159ba3f2b45315e
Shay <seamus.y2016@gmail.com>
2024-11-06 22:11:05 +0800

    added firebase

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ const cors = require("@koa/cors");
 const App = new Koa();
 const port = 8000;
 const router = require("./router");
+
 App.use(parser())
   .use(cors())
   .use(router.routes())
--- Backend/router.js
+++ Backend/router.js
@@ -1,7 +1,45 @@
 const Router = require("koa-router");
 const router = new Router();
+const googleConfig = require("../secret.json");
 const { getBaggages } = require("./controllers/baggage_controllers/baggage.controller");
 const { getPinColor } = require("./controllers/baggage_controllers/lightpin.controller");
+var admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert(googleConfig),
+  databaseURL: "https://testing_db.firebaseio.com"
+});
+
+require('dotenv').config()
+
+// const app = initializeApp({
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//   appId: process.env.APP_ID,
+// });
+
+// const db = getFirestore(app);
+const db = admin.firestore();
+
+
+router.get("/testing_db", async (ctx) => {
+  // const q = query(collection(db, "testing_db"), where("id", "==", "H3LL0"));
+  try {
+    const q = db.collection("/users").doc("cDU392ANN7OVdBYAnN9l");
+    const user = await q.get();
+    const userData = user.data();
+    ctx.body = JSON.stringify(userData);
+    ctx.status = 200;
+  } catch(err) {
+    console.log("ERROR ", err);
+    ctx.body = "Error";
+    ctx.status = 500;
+  }
+});
+
 router.get("/baggages", getBaggages);
 router.post("/add_event", (ctx) => (ctx.body = "Event Posted!"));
 
