a9909b4341b7db9c31d0b8f08f6c45b2b6aebfc7
Albert LAU <32214383+albertlau128@users.noreply.github.com>
2024-11-07 04:48:54 +0800

    getBaggages

--- Backend/controllers/baggage_controllers/baggage.controller.js
+++ Backend/controllers/baggage_controllers/baggage.controller.js
@@ -1,6 +1,16 @@
-const getBaggages = (ctx) => {
-  ctx.body = [];
+const getBaggages = async (ctx) => {
+  try{let baggagedb = await db.collection('/baggage').get()
+  let list = ctx.body = baggagedb.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data()
+  }))
+  ctx.body = list;
   ctx.status = 200;
+  }catch(err){
+    console.log(err)
+    ctx.body = "Error: {" + err + "}";
+    ctx.status = 500
+  }
 };
 
 const updateBaggages = (ctx) => {
